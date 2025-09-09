447383fa87b856e55dc5ff2e368de746e763a110
Alexandr Krikun <44290517+ksanyok@users.noreply.github.com>
2025-09-09 22:36:42 +0300

    `Update version number in db.php and modify footer.php to show update links only for authenticated users.`

--- footer.php
+++ footer.php
@@ -4,8 +4,12 @@
  */
 ?>
 <footer style="text-align: center; padding: 10px; background: #f0f0f0; margin-top: 20px;">
-    <p>Service developed by <a href="https://buyreadysite.com" target="_blank">Buyreadysite.com</a> | OpenParcelTracker v<?= get_version() ?> | Last updated: <?= get_last_update() ?> | <a href="/admin/index.php">Update</a> | <a href="/admin/index.php?force_update=1">Force Update</a></p>
-    <?php if (isset($version_info) && $version_info && $version_info['update_available']): ?>
+    <p>Service developed by <a href="https://buyreadysite.com" target="_blank">Buyreadysite.com</a> | OpenParcelTracker v<?= get_version() ?> | Last updated: <?= get_last_update() ?>
+    <?php if (isset($_SESSION['uid'])): ?>
+    | <a href="/admin/index.php">Update</a> | <a href="/admin/index.php?force_update=1">Force Update</a>
+    <?php endif; ?>
+    </p>
+    <?php if (isset($_SESSION['uid']) && isset($version_info) && $version_info && $version_info['update_available']): ?>
     <p style="color: yellow;">Update to <?php echo htmlspecialchars($version_info['latest']); ?> available</p>
     <?php endif; ?>
 </footer>
